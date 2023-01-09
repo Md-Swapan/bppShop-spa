523cc972c58a9fb6fec674e01ef627494e00ca3f
Md-Swapan <swapan@excelitai.com>
2023-01-09 17:12:13 +0600

    nav and sidebar update

--- src/Components/SharedComponents/Nav/Nav.js
+++ src/Components/SharedComponents/Nav/Nav.js
@@ -7,53 +7,58 @@ const Nav = () => {
     <>
       <div className="navbar-section">
         <nav className="nav">
-          <div className="nav-content">
-            <div className="logo">
-              <img className="bpshopsLogo" src="img/bppshops-logo.png" alt="" />
+          {/* <div className="container-fluid"> */}
+            <div className="nav-content">
+              <div className="logo">
+                <img
+                  className="bpshopsLogo"
+                  src="img/bppshops-logo.png"
+                  alt=""
+                />
 
-              <img className="bpshopsIcon" src="img/bpp_icon.png" alt="" />
-            </div>
+                <img className="bpshopsIcon" src="img/bpp_icon.png" alt="" />
+              </div>
 
-            <div className="searchInput">
-              <input
-                type="text"
-                name=""
-                id="dynamic-placeholder"
-                className="search"
-                placeholder="Search by Product Category"
-              />
-              <span className="searchIcon">
-                <i className="bi bi-search"></i>
-              </span>
-            </div>
+              <div className="searchInput">
+                <input
+                  type="text"
+                  name=""
+                  id="dynamic-placeholder"
+                  className="search"
+                  placeholder="Search by Product Category"
+                />
+                <span className="searchIcon">
+                  <i className="bi bi-search"></i>
+                </span>
+              </div>
 
-            <div className="userProfileTab">
-              <div
-                className="user-profile "
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                {/* {agent.image ? (
+              <div className="userProfileTab">
+                <div
+                  className="user-profile "
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {/* {agent.image ? (
                     <img
                       src={`https://agentapi.bppshop.com.bd/${agent.image}`}
                       alt="profile"
                     />
                   ) : ( */}
-                <img src={defaultAvatar} alt="profile" />
-                {/* )} */}
-              </div>
+                  <img src={defaultAvatar} alt="profile" />
+                  {/* )} */}
+                </div>
 
-              <div className="dropdown-menu profile_dropdown">
-                <div
-                  className="d-flex mx-3"
-                  style={{
-                    borderBottom: "1px solid gray",
-                    padding: "10px 0px",
-                    marginBottom: "10px",
-                  }}
-                >
-                  {/* {agent.image ? (
+                <div className="dropdown-menu profile_dropdown">
+                  <div
+                    className="d-flex mx-3"
+                    style={{
+                      borderBottom: "1px solid gray",
+                      padding: "10px 0px",
+                      marginBottom: "10px",
+                    }}
+                  >
+                    {/* {agent.image ? (
                     <img
                       width="30"
                       height="100%"
@@ -61,24 +66,25 @@ const Nav = () => {
                       alt="profile"
                     />
                   ) : ( */}
-                  <img
-                    width="30"
-                    height="100%"
-                    src={defaultAvatar}
-                    alt="profile"
-                  />
-                  {/* )} */}
+                    <img
+                      width="30"
+                      height="100%"
+                      src={defaultAvatar}
+                      alt="profile"
+                    />
+                    {/* )} */}
 
-                  <h6>name</h6>
+                    <h6>name</h6>
+                  </div>
+                  <Link to="/view-profile">
+                    <li className="dropdown-item">View Profile</li>
+                  </Link>
+                  {/* <li onClick={handleLogout} className="dropdown-item"> */}
+                  <li className="dropdown-item">Logout</li>
                 </div>
-                <Link to="/view-profile">
-                  <li className="dropdown-item">View Profile</li>
-                </Link>
-                {/* <li onClick={handleLogout} className="dropdown-item"> */}
-                <li className="dropdown-item">Logout</li>
               </div>
             </div>
-          </div>
+          {/* </div> */}
         </nav>
       </div>
     </>
--- src/Components/SharedComponents/Sidebar/Sidebar.js
+++ src/Components/SharedComponents/Sidebar/Sidebar.js
@@ -1,5 +1,21 @@
 import React from "react";
 import "./Sidebar.css";
+import islamicIcon from '../../../Assets/Images/icons/islamic-icon-vector-27687758.jpg'
+import groceryIcon from '../../../Assets/Images/icons/grocery.webp'
+import fashionIcon from '../../../Assets/Images/icons/image 40.png'
+import babyCareIcon from '../../../Assets/Images/icons/3731013.png'
+import cosmeticsIcon from '../../../Assets/Images/icons/cosmetic.jpg'
+import shoesIcon from '../../../Assets/Images/icons/shoes.jpg'
+import watchIcon from '../../../Assets/Images/icons/watch.png'
+import eyeCareIcon from '../../../Assets/Images/icons/eyecare.jpg'
+import sportsIcon from '../../../Assets/Images/icons/Sport_balls.svg.png'
+import pharmacyIcon from '../../../Assets/Images/icons/pharmacy-icon-3.jpg'
+import electronicsIcon from '../../../Assets/Images/icons/electronics.jpg'
+import furnitureIcon from '../../../Assets/Images/icons/furniture.jpeg'
+import hardwareIcon from '../../../Assets/Images/icons/image 52 (1).png'
+import homeDecorateIcon from '../../../Assets/Images/icons/image 50.png'
+import vehicleIcon from '../../../Assets/Images/icons/image 53.png'
+import usedProductsIcon from '../../../Assets/Images/icons/usedProduct.jfif'
 
 const Sidebar = () => {
   return (
@@ -13,9 +29,9 @@ const Sidebar = () => {
         </label>
         <div id="sidebarMenu">
           <ul className="menu">
-            <li>
+            <li className="homeIcon">
               <a href="">
-                <i className="bi bi-house-door-fill"></i> Home
+                <i className="bi bi-house-door-fill"></i>
               </a>
             </li>
             <li>
@@ -23,88 +39,196 @@ const Sidebar = () => {
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/islamicicon.png"
+                  src={islamicIcon}
                   alt=""
                 />
-                Islamic Products
+                 Islamic
               </a>
+
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
             <li>
               <a href="">
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/groceryicon.png"
+                  src={groceryIcon}
                   alt=""
                 />
-                Grocery
+                Grocery (only in Dhaka city)
               </a>
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
             <li>
               <a href="">
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/fasionicon.png"
+                  src={fashionIcon}
                   alt=""
                 />
                 Fashion
               </a>
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
             <li>
               <a href="">
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/electronicicon.png"
-                  alt=""
-                />
-                Electronics
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <img
-                  width="15"
-                  className="sidebar-Icon"
-                  src="img/babycareicon.png"
+                  src={babyCareIcon}
                   alt=""
                 />
                 Baby Care
               </a>
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
             <li>
               <a href="">
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/cosmaticicon.png"
+                  src={cosmeticsIcon}
                   alt=""
                 />
                 Cosmetics
               </a>
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
+            
+            
             <li>
               <a href="">
                 <img
                   width="15"
                   className="sidebar-Icon"
-                  src="img/furntureicon.png"
-                  alt=""
-                />
-                Furniture
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <img
-                  width="15"
-                  className="sidebar-Icon"
-                  src="img/shoesicon.png"
+                  src={shoesIcon}
                   alt=""
                 />
                 Shoes
               </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={watchIcon}
+                  alt=""
+                />
+                Watch
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={eyeCareIcon}
+                  alt=""
+                />
+                Eye Care
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={sportsIcon}
+                  alt=""
+                />
+                Sports
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={pharmacyIcon}
+                  alt=""
+                />
+                Pharmacy
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={electronicsIcon}
+                  alt=""
+                />
+                Electronics
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={furnitureIcon}
+                  alt=""
+                />
+                Furniture
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={hardwareIcon}
+                  alt=""
+                />
+                Hardware, Cables & Sanitary
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={homeDecorateIcon}
+                  alt=""
+                />
+                Home Appliance & Decorate
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={vehicleIcon}
+                  alt=""
+                />
+                Vehicle
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
+            </li>
+            <li>
+              <a href="">
+                <img
+                  width="15"
+                  className="sidebar-Icon"
+                  src={usedProductsIcon}
+                  alt=""
+                />
+                Used Products
+              </a>
+              <span><i class="bi bi-chevron-right"></i></span>
             </li>
           </ul>
         </div>
