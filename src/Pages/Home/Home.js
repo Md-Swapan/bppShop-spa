cab556a9ab32a120be50e254a077dd8f8885c958
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-09 14:33:30 +0600

    button design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Nav from "../../Components/SharedComponents/Nav/Nav";
 import Sidebar from "../../Components/SharedComponents/Sidebar/Sidebar";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <Sidebar/>
+      <Sidebar />
+      <div className="header_Container">
+        <div className="header_btn flex justify-center align-middle">
+          <button>Brand</button>
+          <button>Discounted Products</button>
+          <button>Seller Zone</button>
+          <button>Hot Items</button>
+        </div>
+      </div>
     </>
   );
 };
