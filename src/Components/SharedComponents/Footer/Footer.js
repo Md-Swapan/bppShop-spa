f5130fe2c26a3394d62b6873d5768cf728be7251
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-10 15:58:47 +0600

    bottom footer design

--- src/Components/SharedComponents/Footer/Footer.js
+++ src/Components/SharedComponents/Footer/Footer.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import './Footer.css'
-import logo from '../../../Assets/Images/bpp shop logo fainal.png'
+import React from "react";
+import "./Footer.css";
+import logo from "../../../Assets/Images/bpp shop logo fainal.png";
 
 const Footer = () => {
-    return (
-        <>
-           <div className='footer_container_section'>
-            <div className='footer-content'>
-                <div className='brand_address'>
-                    <div className='logo'>
-                        <img src={logo} alt=""/>
-                    </div>
-                    <div className='address'>
-                        <h4>Address</h4>
-                        <p>17 (9th Floor), Alhaz Shamsuddin Mansion, New Eskaton Road, Dhaka 1217</p>
-                    </div>
-                </div>
-                <div className='account_shipping_info'>
-                    <div className='logo'>
-                    ACCOUNT & SHIPPING INFO
-                    </div>
-                    <div className='address'>
-                        <h4>Address</h4>
-                        <p>17 (9th Floor), Alhaz Shamsuddin Mansion, New Eskaton Road, Dhaka 1217</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="footer_container_section">
+        <div className="footer-content">
+          <div className="brand_address">
+            <div className="logo">
+              <img src={logo} alt="" />
             </div>
-           </div>
-           <div className='bottom_footer_container_section text-center'>
-                hi
-           </div>
-        </>
-    );
+            <div className="address">
+              <h4>Address</h4>
+              <p>
+                17 (9th Floor), Alhaz Shamsuddin Mansion, New Eskaton Road,
+                Dhaka 1217
+              </p>
+            </div>
+          </div>
+          <div className="account_shipping_info">
+            <div className="logo">ACCOUNT & SHIPPING INFO</div>
+            <div className="address">
+              <h4>Address</h4>
+              <p>
+                17 (9th Floor), Alhaz Shamsuddin Mansion, New Eskaton Road,
+                Dhaka 1217
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom_footer_container_section">
+        <div className="bootom_footer_containt">
+          <div className="bottom_footer_left">© All Right Reserved BPPSHOPS</div>
+          <div className="bottom_footer_right">
+            <div className="bottom_term_text">Terms & conditions</div>
+            <div className="bottom_privacy_text">Privacy Policy</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
