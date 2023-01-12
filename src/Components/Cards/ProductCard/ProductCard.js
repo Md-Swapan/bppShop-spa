9b8c66dd883111f86d1744578a23de2088a175ed
Md-Swapan <swapan@excelitai.com>
2023-01-12 16:50:06 +0600

    product cart design

--- src/Components/Cards/ProductCard/ProductCard.js
+++ src/Components/Cards/ProductCard/ProductCard.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './ProductCard.css';
+import React from "react";
+import "./ProductCard.css";
 
 const ProductCard = (props) => {
-  const {id, title, image} = props.product;
+  const { id, title, image, price } = props.product;
   return (
     <>
-     <div
-      className="product_card_content"
-    >
-      <div className="card product-card">
-        <div className="card-body product-card-body">
-          <img src={image} className="card-img-top" alt="" />
+      <div className="product_card_content">
+        <div className=" product-card">
+          <div className=" product-card-body">
+            <img src={image} className="card-img-top" alt="" />
+            <div className="product-card-body-content">
+              <small>{title.toString().substring(0, 20)}....</small>
+              <br />
+              <strong>৳ {price}</strong>
+            </div>
+            <div className="quickView_AddToCart_overlay">
+              <div className="overlayAddToCartBtn">
+                <h5>Add <br/> To <br/> Cart</h5>
+                <i class="bi bi-cart-plus-fill"></i>
+              </div>
+              <button><i class="bi bi-eye-fill"></i> <span>Quick View</span></button>
+            </div>
+          </div>
+
+          <div className="card-footer product-card-footer">
+            <button type="">
+              <i class="bi bi-cart-plus"></i> Add To Cart
+            </button>
+          </div>
         </div>
-        <div className="card-footer product-card-footer">{title}</div>
       </div>
-    </div> 
     </>
   );
 };
