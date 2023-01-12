b867097e63c9717d0197b1c7745f96c00bf04f4f
Md-Samirun-Shuvo <shuvo@excelitai.com>
2023-01-12 14:50:02 +0600

    breadcrumb update

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
           {/* <Route path="/login" element={<Login />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Routes path="/" element={<Navigate replace to="/login" />} /> */}
+          
         </Routes>
       </Layout>
     </div>
